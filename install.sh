4696c7537bd6cb464c85043c62b86b7a08a7dee6
Libmuy <root@pve.lan>
2022-03-04 15:02:19 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,21 @@ apt-get update
 apt-get install -y git make || failed "update failed, please check your network"
 git clone https://github.com/libmuy/pve-setting.git || failed "download repo"
 
-pushd pve-setting
+pushd pve-setting > /dev/null
+
+#setting the environment
 make
 
+#add the user account info for commit
 grep -q user .git/config || cat <<EOS >> .git/config
 [user]
         name = Libmuy
         maile = libmuy@gmail.com
 EOS
 
-popd
+
+
+popd > /dev/null
+
+#ending message
+echo "Pve environment setting successfully!"
