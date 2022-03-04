66a1f9d16fb9218ba18f4709f4422aab0a27bbf1
Libmuy <jianing.bi@xacti-co.com>
2022-03-04 14:50:46 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,8 @@ function failed()
 	exit 1
 }
 
-apt-get update && \
-apt-get install -y git make 
-
-if [ "$?" =! "0" ] ; then
-	failed "update failed, please check your network"
-fi
-
-
-
-
+apt-get update
+apt-get install -y git make || failed "update failed, please check your network"
 git clone https://github.com/libmuy/pve-setting.git || failed "download repo"
 
 pushd pve-setting
